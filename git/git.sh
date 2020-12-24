43a3129c0345a55fd73d0acf6840872acf0a87f2
Claudio Scordino <claudio.scordino@gmail.com>
2020-12-24 16:57:20 +0100

    Configuration for various programs

---
+++ git/git.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git config --global core.editor "vim"
+git config --global user.name "Claudio Scordino"
+git config --global color.status.branch yellow
+git config --global merge.tool meld
+git config --global color.diff=auto
+git config --global color.ui=auto
+git config --global alias.co checkout
+git config --global alias.pick cherry-pick
+git config --global alias.slog 'log --pretty=short'
+git config --global push.default simple
+
+
+# SINGLE PATCH (first submission):
+#	git format-patch -s -M -1 -o send
+# SINGLE PATCH (N-th submission):
+#	git format-patch -v N -s -M -1 -o send
+# MULTIPLE PATCHES (first submission):
+#	git format-patch -n -s -M --cover-letter --thread -NUMBER_OF_COMMITS -o send
+# MULTIPLE PATCHES (N-th submission):
+#	git format-patch -v N -n -s -M --cover-letter --thread -NUMBER_OF_COMMITS -o send
