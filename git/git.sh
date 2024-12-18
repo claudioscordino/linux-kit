e19287cffadd78b93c0fb8ae2281361128c6cd52
Claudio Scordino <claudio.scordino@gmail.com>
2024-12-18 12:38:58 +0100

    [git] Fancier git slog output

--- git/git.sh
+++ git/git.sh
@@ -4,11 +4,12 @@ git config --global core.editor "vim"
 git config --global user.name "Claudio Scordino"
 git config --global color.status.branch yellow
 git config --global merge.tool meld
-git config --global color.diff=auto
-git config --global color.ui=auto
+git config --global color.diff auto
+git config --global color.ui auto
 git config --global alias.co checkout
 git config --global alias.pick cherry-pick
-git config --global alias.slog 'log --pretty=short'
+#git config --global alias.slog 'log --pretty=short'
+git config --global alias.slog 'log --pretty=format:"%h %as %<(20)%an%x09 %s"'
 git config --global push.default simple
 
 
